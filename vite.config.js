b2a2a90c425d04cb2282bc5a4796827710244cdc
andersoninnovationlatam <genspark_dev@genspark.ai>
2026-01-28 14:00:01 +0000

    fix: allowedHosts para sandbox

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 3000,
+    allowedHosts: 'all'
   },
   preview: {
     host: '0.0.0.0',
-    port: 3000
+    port: 3000,
+    allowedHosts: 'all'
   }
 })
